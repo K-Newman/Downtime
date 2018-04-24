e9a9de4237e7a6f0c79bc6cf698f54cae55d69ed
K-Newman <publix251>
2018-04-24 18:58:18 -0400

    Added proper button controls Added logic for csv strings

--- app/src/main/java/com/example/kdn00/downtime/MainActivity.java
+++ app/src/main/java/com/example/kdn00/downtime/MainActivity.java
@@ -1,29 +1,37 @@
 package com.example.kdn00.downtime;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class MainActivity extends AppCompatActivity{
     ToggleButton dbR,dbS,dbB,dbI;
     ToggleButton startButton;
+    TextView textView;
+    EditText testOutput;
     long startTime = 0;
     long duration = 0;
-    long runTime = 0;
-    String currentActive = "";
-    //PrintWriter pw = new PrintWriter(new File("test.csv"));
+    long runTime;
+    String dbActive = "";
     StringBuilder sb = new StringBuilder();
+    Date now = new Date();
 
-    //public MainActivity() throws FileNotFoundException {
-    //}
+
+    String name = new SimpleDateFormat("HHmmss").format(now);
+    FileWriter output;
+
 
 
     @Override
@@ -33,47 +41,120 @@ public class MainActivity extends AppCompatActivity{
 
         startButton = findViewById(R.id.startButton);
         dbR = findViewById(R.id.dbR);
+        dbB = findViewById(R.id.dbB);
+        dbS = findViewById(R.id.dbS);
+        dbI = findViewById(R.id.dbI);
 
-        TextView textView = (TextView) findViewById(R.id.UpTime);
-        textView.setText(runTime+"");
 
-    }
-    public void onButtonClick(View view){
-        switch(view.getId()){
-            case R.id.dbR:
-                dbR.setChecked(true);
+        textView = findViewById(R.id.UpTime);
+        testOutput = findViewById(R.id.testOutput);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        dbR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbS.setChecked(false);
+                dbI.setChecked(false);
+                dbB.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append(name);
+                sb.append(dbActive);
+                sb.append(",");
+                sb.append((System.nanoTime() - startTime)/1000000000.0);
+                sb.append("\n");
+                testOutput.setText(sb);
+                startTime = System.nanoTime();
+                dbActive = "Running";
+                try {
+                    output = new FileWriter("test.csv");
+                    output.write(sb.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        dbB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(dbR.isChecked()){
+                    runTime += System.nanoTime() - startTime;
+                    textView.setText(runTime/1000000000.0 + "");
+                }
+                dbR.setChecked(false);
+                dbI.setChecked(false);
+                dbS.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append(name);
+                sb.append(dbActive);
+                sb.append(",");
+                sb.append((System.nanoTime() - startTime)/1000000000.0);
+                sb.append("\n");
+                testOutput.setText(sb);
+                startTime = System.nanoTime();
+                dbActive = "Blocked";
+            }
+        });
+
+        dbS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(dbR.isChecked()){
+                    runTime += System.nanoTime() - startTime;
+                    textView.setText(runTime/1000000000.0 + "");
+                }
+                dbR.setChecked(false);
+                dbI.setChecked(false);
+                dbB.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append(name);
+                sb.append(dbActive);
+                sb.append(",");
+                sb.append((System.nanoTime() - startTime)/1000000000.0);
+                sb.append("\n");
+                testOutput.setText(sb);
+                startTime = System.nanoTime();
+                dbActive = "Starved";
+            }
+        });
+
+        dbI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(dbR.isChecked()){
+                    runTime += System.nanoTime() - startTime;
+                    textView.setText(runTime/1000000000.0 + "");
+                }
+                dbR.setChecked(false);
                 dbS.setChecked(false);
                 dbB.setChecked(false);
-                dbI.setChecked(false);
-                //currentActive = "Debagger,Running";
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append(name);
+                sb.append(dbActive);
+                sb.append(",");
+                sb.append((System.nanoTime() - startTime)/1000000000.0);
+                sb.append("\n");
+                testOutput.setText(sb);
+                startTime = System.nanoTime();
+                dbActive = "Issue";
 
-            case R.id.dbS:
-                if(dbR.isChecked()){runTime += System.nanoTime() - startTime;}
-                dbS.setChecked(true);
-                dbR.setChecked(false);
-                dbB.setChecked(false);
-                dbI.setChecked(false);
-                currentActive = "Debagger,Starved";
-            case R.id.dbB:
-                if(dbR.isChecked()){runTime += System.nanoTime() - startTime;}
-                dbB.setChecked(true);
-                dbS.setChecked(false);
-                dbR.setChecked(false);
-                dbI.setChecked(false);
-                currentActive = "Debagger,Blocked";
-            case R.id.dbI:
-                if(dbR.isChecked()){runTime += System.nanoTime() - startTime;}
-                dbI.setChecked(true);
-                dbS.setChecked(false);
-                dbB.setChecked(false);
-                dbR.setChecked(false);
-                currentActive = "Debagger,Issue";
-        }
-        //duration = System.nanoTime() - startTime;
-        //sb.append(currentActive);
-        //sb.append(",");
-        //sb.append(duration);
-        //startTime = System.nanoTime();
+            }
+        });
+
     }
 
 }
