2ca25af1413573fc452a51c77600376c9f43cce1
K-Newman <publix251>
2018-04-26 17:27:07 -0400

    Added remaining buttons Finalized code App is now in use

--- app/src/main/java/com/example/kdn00/downtime/MainActivity.java
+++ app/src/main/java/com/example/kdn00/downtime/MainActivity.java
@@ -1,37 +1,30 @@
 package com.example.kdn00.downtime;
 
-import android.Manifest;
-import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity{
     ToggleButton dbR,dbS,dbB,dbI;
+    ToggleButton bfR,bfS,bfB,bfI;
+    ToggleButton fR,fS,fB,fI;
+    ToggleButton cpR,cpS,cpB,cpI;
+    ToggleButton pR,pS,pB,pI;
     Button completeButton;
-    TextView textView;
-    long startTime = 0;
-    double duration = 0;
-    long runTime;
-    String dbActive = "";
+    long dbTime,bfTime,fTime,cpTime,pTime = 0;
+    double dbDuration,bfDuration,fDuration,cpDuration,pDuration = 0;
+    String dbActive,bfActive,fActive,cpActive,pActive = "";
     StringBuilder sb = new StringBuilder();
     Date now = new Date();
     generateNoteOnSD body = new generateNoteOnSD();
-
     String name = new SimpleDateFormat("HHmmss").format(now);
-
 
 
     @Override
@@ -40,14 +33,31 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
         completeButton = findViewById(R.id.completeButton);
+
         dbR = findViewById(R.id.dbR);
         dbB = findViewById(R.id.dbB);
         dbS = findViewById(R.id.dbS);
         dbI = findViewById(R.id.dbI);
 
+        bfR = findViewById(R.id.bfR);
+        bfB = findViewById(R.id.bfB);
+        bfS = findViewById(R.id.bfS);
+        bfI = findViewById(R.id.bfI);
 
-        textView = findViewById(R.id.UpTime);
+        fR = findViewById(R.id.fR);
+        fB = findViewById(R.id.fB);
+        fS = findViewById(R.id.fS);
+        fI = findViewById(R.id.fI);
 
+        cpR = findViewById(R.id.cpR);
+        cpB = findViewById(R.id.cpB);
+        cpS = findViewById(R.id.cpS);
+        cpI = findViewById(R.id.cpI);
+
+        pR = findViewById(R.id.pR);
+        pB = findViewById(R.id.pB);
+        pS = findViewById(R.id.pS);
+        pI = findViewById(R.id.pI);
 
         completeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,23 +79,17 @@ public class MainActivity extends AppCompatActivity{
                 sb.append(",");
                 sb.append(dbActive);
                 sb.append(",");
-                duration = (System.nanoTime() - startTime)/1000000000.0;
-                sb.append(duration+"");
+                dbDuration = (System.nanoTime() - dbTime)/1000000000.0;
+                sb.append(dbDuration +"");
                 sb.append("\n");
-                startTime = System.nanoTime();
+                dbTime = System.nanoTime();
                 dbActive = "Running";
-
-
             }
         });
 
         dbB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(dbR.isChecked()){
-                    runTime += System.nanoTime() - startTime;
-                    textView.setText(runTime/1000000000.0 + "");
-                }
                 dbR.setChecked(false);
                 dbI.setChecked(false);
                 dbS.setChecked(false);
@@ -96,10 +100,10 @@ public class MainActivity extends AppCompatActivity{
                 sb.append(",");
                 sb.append(dbActive);
                 sb.append(",");
-                duration = (System.nanoTime() - startTime)/1000000000.0;
-                sb.append(duration+"");
+                dbDuration = (System.nanoTime() - dbTime)/1000000000.0;
+                sb.append(dbDuration +"");
                 sb.append("\n");
-                startTime = System.nanoTime();
+                dbTime = System.nanoTime();
                 dbActive = "Blocked";
             }
         });
@@ -107,10 +111,6 @@ public class MainActivity extends AppCompatActivity{
         dbS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(dbR.isChecked()){
-                    runTime += System.nanoTime() - startTime;
-                    textView.setText(runTime/1000000000.0 + "");
-                }
                 dbR.setChecked(false);
                 dbI.setChecked(false);
                 dbB.setChecked(false);
@@ -121,10 +121,10 @@ public class MainActivity extends AppCompatActivity{
                 sb.append(",");
                 sb.append(dbActive);
                 sb.append(",");
-                duration = (System.nanoTime() - startTime)/1000000000.0;
-                sb.append(duration+"");
+                dbDuration = (System.nanoTime() - dbTime)/1000000000.0;
+                sb.append(dbDuration +"");
                 sb.append("\n");
-                startTime = System.nanoTime();
+                dbTime = System.nanoTime();
                 dbActive = "Starved";
             }
         });
@@ -132,10 +132,6 @@ public class MainActivity extends AppCompatActivity{
         dbI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(dbR.isChecked()){
-                    runTime += System.nanoTime() - startTime;
-                    textView.setText(runTime/1000000000.0 + "");
-                }
                 dbR.setChecked(false);
                 dbS.setChecked(false);
                 dbB.setChecked(false);
@@ -146,15 +142,351 @@ public class MainActivity extends AppCompatActivity{
                 sb.append(",");
                 sb.append(dbActive);
                 sb.append(",");
-                duration = (System.nanoTime() - startTime)/1000000000.0;
-                sb.append(duration+"");
+                dbDuration = (System.nanoTime() - dbTime)/1000000000.0;
+                sb.append(dbDuration +"");
                 sb.append("\n");
-                startTime = System.nanoTime();
+                dbTime = System.nanoTime();
                 dbActive = "Issue";
+            }
+        });
+
+        bfR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bfI.setChecked(false);
+                bfB.setChecked(false);
+                bfS.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append("BoxFormer,");
+                sb.append(name);
+                sb.append(",");
+                sb.append(bfActive);
+                sb.append(",");
+                bfDuration = (System.nanoTime() - bfTime)/1000000000.0;
+                sb.append(bfDuration + "");
+                sb.append("\n");
+                bfTime = System.nanoTime();
+                bfActive = "Running";
+            }
+        });
+
+        bfB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bfI.setChecked(false);
+                bfR.setChecked(false);
+                bfS.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append("BoxFormer,");
+                sb.append(name);
+                sb.append(",");
+                sb.append(bfActive);
+                sb.append(",");
+                bfDuration = (System.nanoTime() - bfTime)/1000000000.0;
+                sb.append(bfDuration + "");
+                sb.append("\n");
+                bfTime = System.nanoTime();
+                bfActive = "Blocked";
+            }
+        });
+
+        bfI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bfR.setChecked(false);
+                bfB.setChecked(false);
+                bfS.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append("BoxFormer,");
+                sb.append(name);
+                sb.append(",");
+                sb.append(bfActive);
+                sb.append(",");
+                bfDuration = (System.nanoTime() - bfTime)/1000000000.0;
+                sb.append(bfDuration + "");
+                sb.append("\n");
+                bfTime = System.nanoTime();
+                bfActive = "Issue";
+            }
+        });
+
+        bfS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bfI.setChecked(false);
+                bfB.setChecked(false);
+                bfR.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append("BoxFormer,");
+                sb.append(name);
+                sb.append(",");
+                sb.append(bfActive);
+                sb.append(",");
+                bfDuration = (System.nanoTime() - bfTime)/1000000000.0;
+                sb.append(bfDuration + "");
+                sb.append("\n");
+                bfTime = System.nanoTime();
+                bfActive = "Starved";
+            }
+        });
+
+        fR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fI.setChecked(false);
+                fB.setChecked(false);
+                fS.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append("Filler,");
+                sb.append(name);
+                sb.append(",");
+                sb.append(fActive);
+                sb.append(",");
+                fDuration = (System.nanoTime() - fTime)/1000000000.0;
+                sb.append(fDuration + "");
+                sb.append("\n");
+                fTime = System.nanoTime();
+                fActive = "Running";
+            }
+        });
+
+        fI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fR.setChecked(false);
+                fB.setChecked(false);
+                fS.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append("Filler,");
+                sb.append(name);
+                sb.append(",");
+                sb.append(fActive);
+                sb.append(",");
+                fDuration = (System.nanoTime() - fTime)/1000000000.0;
+                sb.append(fDuration + "");
+                sb.append("\n");
+                fTime = System.nanoTime();
+                fActive = "Issue";
+            }
+        });
+
+        fB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fI.setChecked(false);
+                fR.setChecked(false);
+                fS.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append("Filler,");
+                sb.append(name);
+                sb.append(",");
+                sb.append(fActive);
+                sb.append(",");
+                fDuration = (System.nanoTime() - fTime)/1000000000.0;
+                sb.append(fDuration + "");
+                sb.append("\n");
+                fTime = System.nanoTime();
+                fActive = "Blocked";
+            }
+        });
+
+        fS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fI.setChecked(false);
+                fB.setChecked(false);
+                fR.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append("Filler,");
+                sb.append(name);
+                sb.append(",");
+                sb.append(fActive);
+                sb.append(",");
+                fDuration = (System.nanoTime() - fTime)/1000000000.0;
+                sb.append(fDuration + "");
+                sb.append("\n");
+                fTime = System.nanoTime();
+                fActive = "Starved";
+            }
+        });
+
+        cpR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cpS.setChecked(false);
+                cpI.setChecked(false);
+                cpB.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append("CasePacker,");
+                sb.append(name);
+                sb.append(",");
+                sb.append(cpActive);
+                sb.append(",");
+                cpDuration = (System.nanoTime() - cpTime)/1000000000.0;
+                sb.append(cpDuration +"");
+                sb.append("\n");
+                cpTime = System.nanoTime();
+                cpActive = "Running";
+            }
+        });
+
+        cpS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cpR.setChecked(false);
+                cpI.setChecked(false);
+                cpB.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append("CasePacker,");
+                sb.append(name);
+                sb.append(",");
+                sb.append(cpActive);
+                sb.append(",");
+                cpDuration = (System.nanoTime() - cpTime)/1000000000.0;
+                sb.append(cpDuration +"");
+                sb.append("\n");
+                cpTime = System.nanoTime();
+                cpActive = "Starved";
+            }
+        });
+
+        cpI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cpS.setChecked(false);
+                cpR.setChecked(false);
+                cpB.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append("CasePacker,");
+                sb.append(name);
+                sb.append(",");
+                sb.append(cpActive);
+                sb.append(",");
+                cpDuration = (System.nanoTime() - cpTime)/1000000000.0;
+                sb.append(cpDuration +"");
+                sb.append("\n");
+                cpTime = System.nanoTime();
+                cpActive = "Issue";
+            }
+        });
+
+        cpB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cpS.setChecked(false);
+                cpI.setChecked(false);
+                cpR.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append("CasePacker,");
+                sb.append(name);
+                sb.append(",");
+                sb.append(cpActive);
+                sb.append(",");
+                cpDuration = (System.nanoTime() - cpTime)/1000000000.0;
+                sb.append(cpDuration +"");
+                sb.append("\n");
+                cpTime = System.nanoTime();
+                cpActive = "Blocked";
+            }
+        });
+
+        pR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pS.setChecked(false);
+                pI.setChecked(false);
+                pB.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append("Palletizer,");
+                sb.append(name);
+                sb.append(",");
+                sb.append(pActive);
+                sb.append(",");
+                pDuration = (System.nanoTime() - pTime)/1000000000.0;
+                sb.append(pDuration +"");
+                sb.append("\n");
+                pTime = System.nanoTime();
+                pActive = "Running";
+
 
             }
         });
 
+        pS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pR.setChecked(false);
+                pI.setChecked(false);
+                pB.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append("Palletizer,");
+                sb.append(name);
+                sb.append(",");
+                sb.append(pActive);
+                sb.append(",");
+                pDuration = (System.nanoTime() - pTime)/1000000000.0;
+                sb.append(pDuration +"");
+                sb.append("\n");
+                pTime = System.nanoTime();
+                pActive = "Starved";
+            }
+        });
+
+        pI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pS.setChecked(false);
+                pR.setChecked(false);
+                pB.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append("Palletizer,");
+                sb.append(name);
+                sb.append(",");
+                sb.append(pActive);
+                sb.append(",");
+                pDuration = (System.nanoTime() - pTime)/1000000000.0;
+                sb.append(pDuration +"");
+                sb.append("\n");
+                pTime = System.nanoTime();
+                pActive = "Issue";
+            }
+        });
+
+        pB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pS.setChecked(false);
+                pI.setChecked(false);
+                pR.setChecked(false);
+                now = new Date();
+                name = new SimpleDateFormat("HHmmss").format(now);
+                sb.append("Palletizer,");
+                sb.append(name);
+                sb.append(",");
+                sb.append(pActive);
+                sb.append(",");
+                pDuration = (System.nanoTime() - pTime)/1000000000.0;
+                sb.append(pDuration +"");
+                sb.append("\n");
+                pTime = System.nanoTime();
+                pActive = "Blocked";
+            }
+        });
     }
 
 }
